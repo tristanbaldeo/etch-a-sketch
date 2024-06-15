89ccad73842caef04f3f2921a07491075c5b028b
tristanbaldeo <113404111+tristanbaldeo@users.noreply.github.com>
2024-06-15 14:19:00 -0400

    redid html to better suit final layout design

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const grid = document.querySelector(".grid-container")
+// const grid = document.querySelector(".grid-container")
 
-function makeGrid() {
-    for (i = 0; i < 16; i++) {
-        const row = document.createElement("div");
-        row.className = "row";
-        grid.appendChild(row);
+// function makeGrid() {
+//     for (i = 0; i < 16; i++) {
+//         const row = document.createElement("div");
+//         row.className = "row";
+//         grid.appendChild(row);
 
-    for (j = 0; j < 16; j++) {
-        const col = document.createElement("div");
-        col.className = "col";
-        row.appendChild(col);
-        }
-    }
-}
+//     for (j = 0; j < 16; j++) {
+//         const col = document.createElement("div");
+//         col.className = "col";
+//         row.appendChild(col);
+//         }
+//     }
+// }
 
-makeGrid();
+// makeGrid();
